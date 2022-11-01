d2434477217feaef73f0d35dddf88eaddbc2029d
junhwan <mynameisljh@naver.com>
2022-11-01 23:19:36 +0900

    커밋

---
+++ src/main/java/com/teamproject/checkin/checkin/CheckInApplication.java
@@ -0,0 +1,21 @@
+package com.teamproject.checkin.checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CheckInApplication {
+
+    @RequestMapping("/")
+    public String home(){
+        return "hello SpringBoot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamproject/checkin/checkin/CheckInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.checkin.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
