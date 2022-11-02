7b6080fddc1112cc2156f65c2e60b9048a2819bf
ldh2481@gmail.com <ldh2481@gmail.com>
2022-11-03 00:03:53 +0900

    부트스트랩, 헤더,푸터,스크립트 등등 공통화 처리

--- src/main/java/com/teamproject/checkin/checkin/controller/TestController.java
+++ src/main/java/com/teamproject/checkin/checkin/controller/TestController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
+    //기본부트스트랩
     @GetMapping("/test")
     public String test(Model model){
 
-        model.addAttribute("data","타임리프 테스트");
         return "test";
     }
 
+    //부트스트랩 틀
     @GetMapping("/index")
     public String index(Model model){
 
@@ -21,18 +22,19 @@ public class TestController {
         return "index";
     }
 
+    //헤더푸터 제거
+    @GetMapping("/index2")
+    public String index2(Model model){
+
+        model.addAttribute("data","타임리프 테스트");
+        return "index2";
+    }
+
+    //부트스트랩 틀
     @GetMapping("/hihi")
     public String hihi(Model model){
 
         model.addAttribute("data","타임리프 테스트");
         return "hihi";
     }
-
-    @GetMapping("/layout")
-    public String layout(Model model){
-
-        model.addAttribute("data","타임리프 테스트");
-        return "/layout/layout";
-    }
-
 }
