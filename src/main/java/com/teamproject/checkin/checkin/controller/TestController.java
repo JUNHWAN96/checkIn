b9d6a42513447db6673726a40f7665b34a9736f4
hazy1993 <ldh2481@gmail.com>
2022-11-02 18:21:45 +0900

    2022-11-02 레이아웃 및 부트스트랩 적용

--- src/main/java/com/teamproject/checkin/checkin/controller/TestController.java
+++ src/main/java/com/teamproject/checkin/checkin/controller/TestController.java
@@ -1,4 +1,38 @@
 package com.teamproject.checkin.checkin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+
+    @GetMapping("/test")
+    public String test(Model model){
+
+        model.addAttribute("data","타임리프 테스트");
+        return "test";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model){
+
+        model.addAttribute("data","타임리프 테스트");
+        return "index";
+    }
+
+    @GetMapping("/hihi")
+    public String hihi(Model model){
+
+        model.addAttribute("data","타임리프 테스트");
+        return "hihi";
+    }
+
+    @GetMapping("/layout")
+    public String layout(Model model){
+
+        model.addAttribute("data","타임리프 테스트");
+        return "/layout/layout";
+    }
+
 }
