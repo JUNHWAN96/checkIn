45f486e7c135ac3c9badd9dd869d6f28afa2c810
woo <woo@DESKTOP-J28CV37>
2022-11-08 14:28:54 +0900

    글꼴 적용

---
+++ src/main/java/com/teamproject/checkin/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.teamproject.checkin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    //기본부트스트랩
+    @GetMapping("/home")
+    public String test(Model model){
+        return "home";
+    }
+
+}
